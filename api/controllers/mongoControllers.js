84a5a16df0a40bed4197a3b12a64908bf8be1b8e
Phillip Dodd <pdodd@uga.edu>
2019-09-19 03:10:37 -0400

    wip

--- api/controllers/mongoControllers.js
+++ api/controllers/mongoControllers.js
@@ -2,10 +2,11 @@ const logger = require('../../lib/myWinston')('mongoControllers.js');
 
 exports.create = async (req, res) => {
     try {
-        logger.info(`Receiving post request to create entity: ${req.params.entityType}`);
+        logger.info(`Receiving post request to create entity: ${req.params.entityType} using ${JSON.stringify(req.body)}`);
         let entity = require(`../../models/${req.params.entityType}`);
         await entity.create(req.body);
-        logger.info(`${entity} successfully created with JSON: ${JSON.stringify(req.body)}`);
+        res.json({message: "success"})
+        logger.info(`Successfully created with JSON: ${JSON.stringify(req.body)}`);
     } catch (e) {
         throw e;
     }
@@ -13,7 +14,7 @@ exports.create = async (req, res) => {
 
 exports.query = async (req, res) => {
     logger.info(`Receiving post request to query entity: ${req.params.entityType} with query: ${JSON.stringify(req.body.query)}`);
-    let entity = require(`../../schemas/uga_invoice_item`);
+    let entity = require(`../../models/${req.params.entityType}`);
     res.json({
         results: await entity.find(req.body.query || {})
     });
